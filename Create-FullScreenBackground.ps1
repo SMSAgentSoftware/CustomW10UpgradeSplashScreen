6eedbd021d2a4b01b221d943bbd0f4e1dee4eecc
Trevor Jones <SMSAgentSoftware@users.noreply.github.com>
2018-08-21 09:04:29 +0100

    Add files via upload

--- Create-FullScreenBackground.ps1
+++ Create-FullScreenBackground.ps1
@@ -1,9 +1,13 @@
-﻿# Creates a full screen 'background' and hides the task bar
+﻿# Creates a full screen 'background' styled for a Windows 10 upgrade, and hides the task bar
+# Called by the "Show-OSUpgradeBackground" script
 
 Param($DeviceName)
 
 # Add required assemblies
-Add-Type -AssemblyName PresentationFramework, System.Windows.Forms
+Add-Type -AssemblyName PresentationFramework,PresentationCore,WindowsBase,System.Windows.Forms,System.Drawing
+Add-Type -AssemblyName System.DirectoryServices.AccountManagement
+Add-Type -Path "$PSSCriptRoot\bin\MahApps.Metro.dll"
+Add-Type -Path "$PSSCriptRoot\bin\System.Windows.Interactivity.dll"
 
 # Find screen by DeviceName
 $Screens = [System.Windows.Forms.Screen]::AllScreens
@@ -51,42 +55,157 @@ public class Taskbar
 "@
 Add-Type -ReferencedAssemblies 'System', 'System.Runtime.InteropServices' -TypeDefinition $Source -Language CSharp
 
+# Find the user identity from the domain if possible
+Try
+{
+    $PrincipalContext = [System.DirectoryServices.AccountManagement.PrincipalContext]::new([System.DirectoryServices.AccountManagement.ContextType]::Domain, [System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain())
+    $GivenName = ([System.DirectoryServices.AccountManagement.Principal]::FindByIdentity($PrincipalContext,[System.DirectoryServices.AccountManagement.IdentityType]::SamAccountName,[Environment]::UserName)).GivenName
+    $PrincipalContext.Dispose()
+}
+Catch {}
+
 # Create a WPF window
 $Window = New-Object System.Windows.Window
-$window.Background = [System.Windows.Media.Brushes]::MidnightBlue
+$window.Background = "#012a47"
 $Window.WindowStyle = [System.Windows.WindowStyle]::None
 $Window.ResizeMode = [System.Windows.ResizeMode]::NoResize
 $Window.Foreground = [System.Windows.Media.Brushes]::White
+$window.Topmost = $True
 
 # Get the bounds of the primary screen
 $Bounds = $Screen.Bounds
 
-# Create a stackpanel container
-$Stackpanel = New-Object System.Windows.Controls.Stackpanel
-$Stackpanel.VerticalAlignment = "Center"
-$Stackpanel.Margin = "0,$($Bounds.Height / 100 * 40),0,0"
+# Assemble a grid
+$Grid = New-object System.Windows.Controls.Grid
+$Grid.Width = "NaN"
+$Grid.Height = "NaN"
+$Grid.HorizontalAlignment = "Stretch"
+$Grid.VerticalAlignment = "Stretch"
+
+# Add a column
+$Column = New-Object System.Windows.Controls.ColumnDefinition
+$Grid.ColumnDefinitions.Add($Column)
+
+# Add rows
+$Row = New-Object System.Windows.Controls.RowDefinition
+$Row.Height = "1*"
+$Grid.RowDefinitions.Add($Row)
+$Row = New-Object System.Windows.Controls.RowDefinition
+$Row.Height = [System.Windows.GridLength]::Auto
+$Grid.RowDefinitions.Add($Row)
+$Row = New-Object System.Windows.Controls.RowDefinition
+$Row.Height = [System.Windows.GridLength]::Auto
+$Grid.RowDefinitions.Add($Row)
+$Row = New-Object System.Windows.Controls.RowDefinition
+$Row.Height = "1*"
+$Grid.RowDefinitions.Add($Row)
+
+# Add a progress ring
+$ProgressRing = [MahApps.Metro.Controls.ProgressRing]::new()
+$ProgressRing.Opacity = 0
+$ProgressRing.IsActive = $false
+$ProgressRing.Margin = "0,0,0,60"
+$Grid.AddChild($ProgressRing)
+$ProgressRing.SetValue([System.Windows.Controls.Grid]::RowProperty,1)
 
 # Add a textblock
 $TextBlock = New-Object System.Windows.Controls.TextBlock
-$TextBlock.Text = "Windows is being upgraded. Do not turn off your computer."
+If ($GivenName)
+{
+    $TextBlock.Text = "Hi $GivenName"
+}
+Else
+{
+    $TextBlock.Text = "Hi there"
+}
 $TextBlock.TextWrapping = [System.Windows.TextWrapping]::Wrap
 $TextBlock.MaxWidth = $Bounds.Width
-$TextBlock.Padding = 10
+$TextBlock.Margin = "0,0,0,120"
 $TextBlock.FontSize = 50
-$TextBlock.VerticalAlignment = "Center"
+$TextBlock.FontWeight = [System.Windows.FontWeights]::Light
+$TextBlock.VerticalAlignment = "Top"
 $TextBlock.HorizontalAlignment = "Center"
-$Stackpanel.AddChild($TextBlock)
+$TextBlock.Opacity = 0
+$Grid.AddChild($TextBlock)
+$TextBlock.SetValue([System.Windows.Controls.Grid]::RowProperty,2)
 
-# Add a progress bar
-$ProgressBar = New-object System.Windows.Controls.ProgressBar
-$ProgressBar.Margin = "0,50,0,0"
-$ProgressBar.IsIndeterminate = $True
-$ProgressBar.Width = ($Bounds.Width / 100 * 70)
-$ProgressBar.Height = 20
-$Stackpanel.AddChild($ProgressBar)
+# Add a textblock
+$TextBlock2 = New-Object System.Windows.Controls.TextBlock
+$TextBlock2.Margin = "0,0,0,60"
+$TextBlock2.Text = "Don't turn off your pc"
+$TextBlock2.TextWrapping = [System.Windows.TextWrapping]::Wrap
+$TextBlock2.MaxWidth = $Bounds.Width
+$TextBlock2.FontSize = 25
+$TextBlock2.FontWeight = [System.Windows.FontWeights]::Light
+$TextBlock2.VerticalAlignment = "Bottom"
+$TextBlock2.HorizontalAlignment = "Center"
+$TextBlock2.Opacity = 0
+$Grid.AddChild($TextBlock2)
+$TextBlock2.SetValue([System.Windows.Controls.Grid]::RowProperty,3)
 
 # Add to window
-$Window.AddChild($Stackpanel)
+$Window.AddChild($Grid)
+
+# Create some animations
+$FadeinAnimation = [System.Windows.Media.Animation.DoubleAnimation]::new(0,1,[System.Windows.Duration]::new([Timespan]::FromSeconds(3)))
+$FadeOutAnimation = [System.Windows.Media.Animation.DoubleAnimation]::new(1,0,[System.Windows.Duration]::new([Timespan]::FromSeconds(3)))
+$ColourBrighterAnimation = [System.Windows.Media.Animation.ColorAnimation]::new("#012a47","#1271b5",[System.Windows.Duration]::new([Timespan]::FromSeconds(5)))
+$ColourDarkerAnimation = [System.Windows.Media.Animation.ColorAnimation]::new("#1271b5","#012a47",[System.Windows.Duration]::new([Timespan]::FromSeconds(5)))
+
+# An array of sentences to display, in order. Leave the first one blank as the 0 index gets skipped.
+$TextArray = @(
+    ""
+    "We're upgrading you to Windows 10 1803"
+    "It may take 30-60 minutes"
+    "Your pc will restart a few times"
+    "Should anything go wrong (we hope it won't)..."
+    "...please give the Helpdesk a call"
+    "Now might be a good time to get a coffee :)"
+    "We'll have you up and running again in no time"
+)
+
+# Start a dispatcher timer. This is used to control when the sentences are changed.
+$TimerCode = {
+    
+    # The IF statement number should equal the number of sentences in the TextArray
+    If ($i -lt 7)
+    {
+        $FadeoutAnimation.Add_Completed({            
+            $TextBlock.Opacity = 0
+            $TextBlock.Text = $TextArray[$i]
+            $TextBlock.BeginAnimation([System.Windows.Controls.TextBlock]::OpacityProperty,$FadeinAnimation)
+
+        })   
+        $TextBlock.BeginAnimation([System.Windows.Controls.TextBlock]::OpacityProperty,$FadeoutAnimation)   
+    }
+    # The final sentence to display ongoing
+    ElseIf ($i -eq 7)
+    {
+        
+        $FadeoutAnimation.Add_Completed({            
+            $TextBlock.Opacity = 0
+            $TextBlock.Text = "Windows 10 Upgrade in Progress"
+            $TextBlock.BeginAnimation([System.Windows.Controls.TextBlock]::OpacityProperty,$FadeinAnimation)
+            $ProgressRing.IsActive = $True
+
+        })   
+        $TextBlock.BeginAnimation([System.Windows.Controls.TextBlock]::OpacityProperty,$FadeoutAnimation)
+    }
+    Else
+    {}
+
+    $ColourBrighterAnimation.Add_Completed({            
+        $Window.Background.BeginAnimation([System.Windows.Media.SolidColorBrush]::ColorProperty,$ColourDarkerAnimation)
+    })   
+    $Window.Background.BeginAnimation([System.Windows.Media.SolidColorBrush]::ColorProperty,$ColourBrighterAnimation)
+
+    $Script:i++
+
+}
+$DispatcherTimer = New-Object -TypeName System.Windows.Threading.DispatcherTimer
+$DispatcherTimer.Interval = [TimeSpan]::FromSeconds(10)
+$DispatcherTimer.Add_Tick($TimerCode)
+
 
 # Event: Window loaded
 $Window.Add_Loaded({
@@ -104,22 +223,40 @@ $Window.Add_Loaded({
     # Hide the taskbar
     [TaskBar]::Hide()
 
+    # Hide the mouse cursor
+    [System.Windows.Forms.Cursor]::Hide()
+
+    # Begin animations
+    $TextBlock.BeginAnimation([System.Windows.Controls.TextBlock]::OpacityProperty,$FadeinAnimation)
+    $TextBlock2.BeginAnimation([System.Windows.Controls.TextBlock]::OpacityProperty,$FadeinAnimation)
+    $ProgressRing.BeginAnimation([System.Windows.Controls.TextBlock]::OpacityProperty,$FadeinAnimation)
+    $ColourBrighterAnimation.Add_Completed({            
+        $Window.Background.BeginAnimation([System.Windows.Media.SolidColorBrush]::ColorProperty,$ColourDarkerAnimation)
+    })   
+    $Window.Background.BeginAnimation([System.Windows.Media.SolidColorBrush]::ColorProperty,$ColourBrighterAnimation)
+
 })
 
 # Event: Window closing (for testing)
-#$Window.Add_Closing({
-#
+$Window.Add_Closing({
+
     # Restore the taskbar
-#    [Taskbar]::Show()
-#
-#})
+    [Taskbar]::Show()
+
+    # Restore the mouse cursor
+    [System.Windows.Forms.Cursor]::Show()
+
+    $DispatcherTimer.Stop()
+
+})
 
 # Event: Close the window on right-click (for testing)
-#$Window.Add_MouseRightButtonDown({
-#
-#    $This.Close()
-#
-#})
+$Window.Add_MouseRightButtonDown({
+
+    $This.Close()
+
+})
 
 # Display the window
+$DispatcherTimer.Start()
 $Window.ShowDialog()
--- Show-OSUpgradeBackground.ps1
+++ Show-OSUpgradeBackground.ps1
@@ -1,4 +1,7 @@
-﻿# Displays a full screen background on each active screen
+﻿# Calls the script that creates the OS upgrade background into a runspace, one per detected screen
+
+#$Location = $PSScriptRoot
+#$Location = "C:\Users\tjones\Desktop\WaaS 1803 Scripts"
 
 # Add required assemblies
 Add-Type -AssemblyName System.Windows.Forms
@@ -7,14 +10,13 @@ Add-Type -AssemblyName System.Windows.Forms
 $Screens = [System.Windows.Forms.Screen]::AllScreens
 
 # Create a runspace to initiate powershell for each screen and call the main script (otherwise each window will only open when the first closes due to the .ShowDialog() method)
-Foreach ($Screen in $screens)
-{
+Foreach ($Screen in $screens) { 
     $PowerShell = [Powershell]::Create()
-    $PowerShell.AddScript({Param($ScriptLocation, $DeviceName); powershell.exe -ExecutionPolicy Bypass -WindowStyle Hidden -File "$ScriptLocation\Create-FullScreenBackground.ps1" -DeviceName $DeviceName})
-    $PowerShell.AddArgument($PSScriptRoot)
-    $PowerShell.AddArgument($Screen.DeviceName)
-    $PowerShell.BeginInvoke()
+    [void]$PowerShell.AddScript({Param($ScriptLocation, $DeviceName); powershell.exe -ExecutionPolicy Bypass -WindowStyle Hidden -File "$ScriptLocation\Create-FullScreenBackground.ps1" -DeviceName $DeviceName})
+    [void]$PowerShell.AddArgument($PSScriptRoot)
+    [void]$PowerShell.AddArgument($Screen.DeviceName)
+    [void]$PowerShell.BeginInvoke()
 }
 
 # Wait for runspace execution
-Start-Sleep -Seconds 5
+Start-Sleep -Seconds 10
