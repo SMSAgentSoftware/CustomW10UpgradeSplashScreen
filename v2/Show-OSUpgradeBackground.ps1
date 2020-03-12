d97c15d79772529e088e96be4b09fec3dde29553
Trevor Jones <SMSAgentSoftware@users.noreply.github.com>
2020-03-12 11:59:13 +0000

    Fix issue with W10 1909 / ConfigMgr Task Sequences
    Fixes an issue with the splash screens closing shortly after starting when running as a ConfigMgr task sequence on Windows 10 1909+

--- v2/Show-OSUpgradeBackground.ps1
+++ v2/Show-OSUpgradeBackground.ps1
@@ -1,19 +1,8 @@
-﻿# Calls the script that creates the OS upgrade background into a runspace, one per detected screen
+﻿# Create a new PS process to call the "Show-OSUpgradeBackground" script, to avoid blocking the continuation of task sequence
 
-# Add required assemblies
-Add-Type -AssemblyName System.Windows.Forms
-
-# Get active screens
-$Screens = [System.Windows.Forms.Screen]::AllScreens
-
-# Create a runspace to initiate powershell for each screen and call the main script (otherwise each window will only open when the first closes due to the .ShowDialog() method)
-Foreach ($Screen in $screens) { 
-    $PowerShell = [Powershell]::Create()
-    [void]$PowerShell.AddScript({Param($ScriptLocation, $DeviceName); powershell.exe -ExecutionPolicy Bypass -WindowStyle Hidden -File "$ScriptLocation\Create-FullScreenBackground.ps1" -DeviceName $DeviceName})
-    [void]$PowerShell.AddArgument($PSScriptRoot)
-    [void]$PowerShell.AddArgument($Screen.DeviceName)
-    [void]$PowerShell.BeginInvoke()
-}
-
-# Wait for runspace execution
-Start-Sleep -Seconds 10
+$Process = New-Object System.Diagnostics.Process
+$Process.StartInfo.UseShellExecute = $false
+$Process.StartInfo.FileName = "PowerShell.exe"
+$Process.StartInfo.Arguments = " -File ""$PSScriptRoot\Create-Runspaces.ps1"""
+$Process.StartInfo.CreateNoWindow = $true
+$Process.Start()
